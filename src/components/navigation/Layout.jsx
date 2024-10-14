f0aeeccb0e04a8d8bb564f8745c3c2b0690171d4
kalvin <kalvin@pezesha.com>
2024-10-15 00:41:34 +0300

    remove unused code

--- src/components/navigation/Layout.jsx
+++ src/components/navigation/Layout.jsx
@@ -1,6 +1,5 @@
 import Footer from "../Footer";
 import Navbar from "./Navbar"
-// import Footer from "./Footers";
 
 const Layout = ({ children }) => {
 
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import '../../Styles.css'
 import HeroSection from '../HeroSection'
-import Navbar from '../navigation/Navbar'
 import Layout from '../navigation/Layout'
 import Cards from '../Cards'
 
