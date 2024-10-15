1a798ac60e208821b5aa85e1b82464a8c4f35200
kalvin <kalvin@pezesha.com>
2024-10-15 11:23:33 +0300

    change navbar menu

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = () => {
             </li> */}
             <li className="nav-item">
               <Link to='/prices' className='nav-links' onClick={closeMobileMenu}>
-                Affordable Prices
+                Prices
               </Link>
             </li>
           </ul>
